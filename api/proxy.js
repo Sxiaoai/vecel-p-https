91cec98d1e63719b18d57da61fb9de5d466f57b3
Sxiaoai <155626436+Sxiaoai@users.noreply.github.com>
2026-03-17 18:28:34 +0800

    Update proxy target logic for environment variables

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = target = process.env.INNER_TARGET_URL || "http://106.15.2.32:6969"; // 内网服务器地址也可通过环境变量配置，同样保留默认值
-  // process.env.DEFAULT_TARGET_URL || "https://www.google.com/"; //你的网址
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = process.env.INNER_TARGET_URL || "http://106.15.2.32:6969"; // 内网服务器地址也可通过环境变量配置，同样保留默认值
-  //   }
+  let target = target = process.env.DEFAULT_TARGET_URL || "https://www.google.com/"; //你的网址
+     if (
+       req.url.startsWith("/api") ||
+       req.url.startsWith("/auth") ||
+       req.url.startsWith("/banner") ||
+       req.url.startsWith("/CollegeTask")
+     ) {
+       target = process.env.INNER_TARGET_URL || "http://106.15.2.32:6969"; // 内网服务器地址也可通过环境变量配置，同样保留默认值
+     }
 
   createProxyMiddleware({
     target,
